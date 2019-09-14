af2736cd9a4a47800f87e86ae149eb6f3aefbf1b
João <jm-yoko@hotmail.com>
2019-09-14 15:29:07 +0100

    Formulário de contacto

--- src/app/contactos/contactos.component.ts
+++ src/app/contactos/contactos.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactos.component.css']
 })
 export class ContactosComponent implements OnInit {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  processForm() {
+    const allInfo = `My name is ${this.name}. My email is ${this.email}. My subject is ${this.subject}. My message is ${this.message}`;
+    alert(allInfo);
   }
 
 }
