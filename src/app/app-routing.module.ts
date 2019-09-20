2efca5619cf1d9e5bca0b6785a4d8e6a07713162
Tiago Silva <tiago.slv@gmail.com>
2019-09-20 09:33:12 +0100

    componentes em falta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,14 @@ import { ServicosComponent } from './servicos/servicos.component';
 import { ContactosComponent } from './contactos/contactos.component';
 import { HomeComponent } from './home/home.component';
 import { TempoComponent } from './tempo/tempo.component';
-
+import { MissaoComponent } from './missao/missao.component';
+import { ValoresComponent } from './valores/valores.component';
+import { EquipaComponent } from './equipa/equipa.component';
 
 const routes: Routes = [
  {path: '', redirectTo: '/home', pathMatch: 'full'},
  {path: 'home', component: HomeComponent},
- {path: 'quemSomos',
- component: QuemSomosComponent,
+ {path: 'quemSomos', component: QuemSomosComponent,
  children: [
    {path: '', redirectTo: 'missao', pathMatch: 'full'},
    {path: 'missao', component: MissaoComponent},
