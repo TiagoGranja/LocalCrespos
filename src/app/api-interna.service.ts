03fdc0608fc76bfeee3db927107811501ddd1276
Tiago Silva <tiago.slv@gmail.com>
2019-10-04 10:21:44 +0100

    Actualizações API interna

--- src/app/api-interna.service.ts
+++ src/app/api-interna.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Cliente } from './models/cliente';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,21 +11,32 @@ export class ApiInternaService {
 
   //getNomeCliente: string = 'nome';
 
-  urlCliente = 'https://localhost:44387/api/cliente';
+  urlCliente = 'https://localhost:44387/api/cliente'; //2º variable which stores the address of the remote API server
   urlTipoQuartoTodos = 'https://localhost:44387/api/quarto/desc';
   urlTipoQuartoSingle = 'https://localhost:44387/api/quarto/desc';
   urlTipoQuartoDouble = 'https://localhost:44387/api/quarto/desc';
   urlTipoQuartoApartamento = 'https://localhost:44387/api/quarto/desc';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }//1º inject HttpClient as a private httpClient instance in the service's constructor
 
-  //função que solicita API que fornece os nosmes dos clientes
+  //3º add the methods for doing create, read, update and delete operations against the the www.server.com/api/customers endpoint.
+
+  //função GET que solicita API que fornece os nosmes dos clientes
   clienteGet() {//o serviço está a fazer o pedido. A forma como vai ser feito é definido no receptor.ts
     console.log(this.urlCliente);
     return this.http.get(this.urlCliente);
     //return this.http.get<any[]>(this.urlCliente);
   }
 
+  public createCliente(cliente: Cliente){}
+
+  public updateCliente(cliente: Cliente){}
+
+  public deleteCliente(id: number){}
+
+  public getClienteById(id: number){}
+
+  public getCliente(url?: string){}
 
   //----------------------- GET - TIPOS DE QUARTO ---------------------------------------------------
 
