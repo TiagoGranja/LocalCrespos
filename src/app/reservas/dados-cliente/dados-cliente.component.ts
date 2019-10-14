a2de4ffea06cceb04c2a415cb3b490e24f19ac37
João <jm-yoko@hotmail.com>
2019-10-14 14:59:55 +0100

    Update Reservas

--- src/app/reservas/dados-cliente/dados-cliente.component.ts
+++ src/app/reservas/dados-cliente/dados-cliente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiInternaService } from 'src/app/api-interna.service';
+import { Cliente } from 'src/app/models/cliente';
 
 @Component({
   selector: 'app-dados-cliente',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dados-cliente.component.css']
 })
 export class DadosClienteComponent implements OnInit {
+  cliente: Cliente; // esta propriedade é um objecto do tipo Cliente
 
-  constructor() { }
+    // Variáveis para o ngModel para criar um cliente novo
+    codCliente: number;
+    telefone: string;
+    email: string;
+    nome: string;
+    dataNasc: any;
+    genero: string;
+
+    clienteServiceCaminho: ApiInternaService;
+  constructor(private clienteService: ApiInternaService) {
+    this.clienteServiceCaminho = clienteService;
+   }
 
   ngOnInit() {
   }
 
+
+    // Função do botão POST "Adicionar Cliente" que guarda, através do serviço da API Interna, os dados de um novo cliente na BD
+    onCreatePost(postData: Cliente) {
+      this.clienteServiceCaminho.createCliente(new Cliente(
+        this.codCliente, this.telefone, this.email, this.nome, this.dataNasc, this.genero)).subscribe(
+        responseData => {
+          console.log(responseData); }
+        );
+    }
 }
